e7f1f47835e3271299b9f9d879f3b349e058b421
C. Carouge <c.carouge@unsw.edu.au>
2017-09-11 10:34:25 +1000

    WRFV_3.9.1.1 from NCAR tar file

--- WRFV3/arch/Config_new.pl
+++ WRFV3/arch/Config_new.pl
@@ -247,7 +247,7 @@ while ( substr( $ARGV[0], 0, 1 ) eq "-" )
 # environment variables JASPERLIB and JASPERINC to paths to library and 
 # include files to enable this feature prior to running configure.  
 
- $I_really_want_to_output_grib2_from_WRF = "TRUE" ;
+ $I_really_want_to_output_grib2_from_WRF = "FALSE" ;
 
  if ( $ENV{JASPERLIB} && $ENV{JASPERINC} && $I_really_want_to_output_grib2_from_WRF eq "TRUE" )
    {
@@ -842,7 +842,7 @@ print "$ENV{WRF_MARS}" ;
 
 close CONFIGURE_WRF ;
 
-printf "Configuration successful! To build the model, please run: qsub run_compile. \n" ;
+printf "Configuration successful! \n" ;
 printf "------------------------------------------------------------------------\n" ;
 
 
